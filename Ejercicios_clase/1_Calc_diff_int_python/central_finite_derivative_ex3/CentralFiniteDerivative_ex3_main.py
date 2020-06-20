cf2cba3aecb978743449d5392a69c57773d760d4
Roberto919 <robertosysm@gmail.com>
2020-06-19 21:15:32 -0500

    Homework (Pracrica 2) advancements (June 19 2020)

---
+++ Ejercicios_clase/1_Calc_diff_int_python/central_finite_derivative_ex3/CentralFiniteDerivative_ex3_main.py
@@ -0,0 +1,67 @@
+## CENTRAL FINITE DIFFERENCES EXCERCISE - MAIN EXECUTION FILE
+#### Based on excercise from file *1_aproximacion_a_derivadas_e_integrales*
+#### Author: Rob (GitHub: Roberto919)
+#### Date: 19 June 2020
+
+
+
+
+
+'--------------------------------------------------------------------------------'
+#############
+## Imports ##
+#############
+
+
+## Python libraries
+import numpy as np
+
+## Ancillary modules
+# from CentralFiniteDerivative_ex3_functions import approx_first_derivative \
+#                                                   approx_second_derivative
+from CentralFiniteDerivative_ex3_functions import *
+from CentralFiniteDerivative_ex3_parameters import fun, x, h
+
+
+
+
+
+'--------------------------------------------------------------------------------'
+#################################
+## Definition of main function ##
+#################################
+
+
+## Main module's function
+def main_function():
+    """
+    Main module's function
+        args:
+            -
+        returns:
+            -
+    """
+
+
+    ## Obtain the first derivative approximation
+    print(approx_first_derivative(fun, x, h))
+
+    ## Obtain the second derivative approximation
+    print(approx_second_derivative(fun, x, h))
+
+
+    return()
+
+
+
+
+
+'--------------------------------------------------------------------------------'
+#################################
+## Excecution of main function ##
+#################################
+
+
+if __name__ == '__main__':
+
+    main_function()
